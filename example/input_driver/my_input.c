a209bb78026672685f5edec65f0ded5e1d591961
anbency <bency140@163.com>
2017-04-17 22:39:00 +0800

    enhance for input device driver

--- example/input_driver/my_input.c
+++ example/input_driver/my_input.c
@@ -14,11 +14,25 @@
 //#define __devexit_p
 
 #define MAX_12BIT       ((1 << 12) - 1)
-
+/*
+static int buttons_irq[1];  
+  
+struct irq_des  
+{  
+    int *buttons_irq;  
+    char *name[1];  
+};  
+  
+  
+struct irq_des button_irqs = {   
+    .buttons_irq = buttons_irq,  
+    .name = {"KEY0"},  
+};  
+*/
 struct my_input {
     struct input_dev    *input;
-    //void __iomem        *my_input_base;
-    //unsigned int        irq;
+    void __iomem        *my_input_base;
+    unsigned int        irq;
     unsigned int        wires;
     unsigned int        x_plate_resistance;
 };
@@ -27,15 +41,38 @@ struct my_input_data {
     int wires;
     int x_plate_resistance;
 };
+static irqreturn_t my_input_irq(int irq, void *dev)
+{
+    //do nothing now
+    return IRQ_HANDLED;
+}
 
 static int __devinit my_input_probe(struct platform_device *pdev)
 {
     const struct my_input_data *pdata = pdev->dev.platform_data;
-    //struct resource *res;
+    struct resource *res;
     struct my_input *my_input_dev;
     struct input_dev *input_dev;
     int err;
+    int irq;
     printk("my input device probe start\n");
+
+    if (!pdata) {
+        dev_err(&pdev->dev, "missing platform data.\n");
+        return -EINVAL;
+    }
+    printk("platform data. wires = %d\n", pdata->wires);
+    res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+    if (!res) {
+        dev_err(&pdev->dev, "no memory resource defined.\n");
+        return -EINVAL;
+    }
+
+    irq = platform_get_irq(pdev, 0);
+    if (irq < 0) {
+        dev_err(&pdev->dev, "no irq ID is specified.\n");
+        return -EINVAL;
+    }
 
     /* Allocate memory for device */
     my_input_dev = kzalloc(sizeof(struct my_input), GFP_KERNEL);
@@ -47,10 +84,32 @@ static int __devinit my_input_probe(struct platform_device *pdev)
     }
 
     my_input_dev->input = input_dev;
-    //my_input_dev->irq = irq;
+    my_input_dev->irq = irq;
     //It will get kernel panic if no platform data - pdata
-    //my_input_dev->wires = pdata->wires;
-    //my_input_dev->x_plate_resistance = pdata->x_plate_resistance;
+    my_input_dev->wires = pdata->wires;
+    my_input_dev->x_plate_resistance = pdata->x_plate_resistance;
+
+    res = request_mem_region(res->start, resource_size(res), pdev->name);
+    if (!res) {
+        dev_err(&pdev->dev, "failed to reserve registers.\n");
+        err = -EBUSY;
+        goto err_free_mem;
+    }
+
+    my_input_dev->my_input_base = ioremap(res->start, resource_size(res));
+    if (!my_input_dev->my_input_base) {
+        dev_err(&pdev->dev, "failed to map registers.\n");
+        err = -ENOMEM;
+        goto err_release_mem_region;
+    }
+    printk("irq num is 0x%x\n", my_input_dev->irq);
+    err = request_irq(my_input_dev->irq, my_input_irq,
+              0, pdev->dev.driver->name, my_input_dev);
+    if (err) {
+        dev_err(&pdev->dev, "failed to allocate irq. err = %d\n", err);
+        goto err_unmap_regs;
+    }
+
     input_dev->name = "my_input";
     input_dev->dev.parent = &pdev->dev;
     input_dev->evbit[0] = BIT_MASK(EV_KEY) | BIT_MASK(EV_ABS);
@@ -73,11 +132,11 @@ err_disable_clk:
     //clk_disable(my_input_dev->tsc_ick);
     //clk_put(my_input_dev->tsc_ick);
 err_free_irq:
-    //free_irq(my_input_dev->irq, ts_dev);
+    free_irq(my_input_dev->irq, my_input_dev);
 err_unmap_regs:
-    //iounmap(my_input_dev->tsc_base);
+    iounmap(my_input_dev->my_input_base);
 err_release_mem_region:
-    //release_mem_region(res->start, resource_size(res));
+    release_mem_region(res->start, resource_size(res));
 err_free_mem:
     printk("my input device probe fail\n");
     input_free_device(input_dev);
@@ -88,15 +147,15 @@ err_free_mem:
 static int __devexit my_input_remove(struct platform_device *pdev)
 {
     struct my_input *my_input_dev = platform_get_drvdata(pdev);
-    //struct resource *res;
+    struct resource *res;
 
-    //free_irq(my_input_dev->irq, my_input_dev);
+    free_irq(my_input_dev->irq, my_input_dev);
 
     input_unregister_device(my_input_dev->input);
 
-    //res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-    //iounmap(my_input_dev->tsc_base);
-    //release_mem_region(res->start, resource_size(res));
+    res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+    iounmap(my_input_dev->my_input_base);
+    release_mem_region(res->start, resource_size(res));
 
 
     kfree(my_input_dev);
--- example/input_driver/my_input_device.c
+++ example/input_driver/my_input_device.c
@@ -17,32 +17,53 @@
 #include <linux/miscdevice.h>  
 #include <linux/sched.h>  
 #include <linux/gpio.h>  
-  
+#include <linux/slab.h>
+#include <mach/motherboard.h>
+
 static struct resource key_resource[]=  
 {     
-    [0] = {  
-        .start = 0x10009000,  
-        .end = 0x10009000,  
+    [0] = {
+        .start = (0x0020000),
+        .end = (0x0020000) + (0x0008000),
+        .flags = IORESOURCE_MEM,
+    },
+    [1] = {  
+        .start = IRQ_V2M_CLCD,  
+        .end = IRQ_V2M_CLCD,  
         .flags = IORESOURCE_IRQ,  
-    },   
+    },
+};
+static struct my_input_data {
+    int wires;
+    int x_plate_resistance;
 };  
-  
-struct platform_device *my_input_dev;  
+struct platform_device *my_input_dev;
   
 static int __init platform_dev_init(void)  
 {  
-    int ret;  
-      
+    int ret;
+    struct my_input_data *pdata;
+
+    pdata = kzalloc(sizeof(*pdata), GFP_KERNEL);
+    if (!pdata)
+        return -ENOMEM;
+       
+    pdata->wires = 4;
+    pdata->x_plate_resistance = 0;
+
     my_input_dev = platform_device_alloc("my_input", -1);  
       
-    platform_device_add_resources(my_input_dev,key_resource,1);//添加资源一定要用该函数，不能使用对platform_device->resource幅值  
-                                                                //否则会导致platform_device_unregister调用失败，内核异常。  
-      
+    platform_device_add_resources(my_input_dev,key_resource,2);//添加资源一定要用该函数，不能使用对platform_device->resource幅值  
+
+    my_input_dev->dev.platform_data = pdata;
+
     ret = platform_device_add(my_input_dev);  
       
     if(ret)  
         platform_device_put(my_input_dev);  
-      
+    
+    kfree(pdata);
+
     return ret;  
 }  
   
